eda3431e3677b941cf04e777fae6cef3c5bd97d3
John Sanchez <jostsace@estudiantec.cr>
2023-10-30 19:06:22 -0600

    Estructura final
    Realizacion de las estructura de carpetas y archivos necesarios para el manejo del proyecto

---
+++ app/config.php
@@ -0,0 +1,26 @@
+<?php 
+    // CONTIENE LAS CONFIGURACIONES GENERALES DEL PROYECTO
+
+    //config de acceso de la base de datos
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'root');
+    // define('DB_PASS', '');
+    // define('DB_NAME', 'clickship');
+
+    //Ruta de la app
+    define('APP_PATH', dirname(dirname(__FILE__))); 
+    //Ruta de url
+    //Ejemplo: http://localhost/website/
+    //define('URL_PATH', 'https://asedocr.com/'); 
+    define('URL_PATH', 'http://localhost/clickship/');
+
+    define('WEB_NAME', 'CLICKSHIP');
+
+    //DEFINICION DE LA ZONA HORARIA
+    date_default_timezone_set('America/Costa_Rica');
+
+    //  INICIALIZAR LAS SESIONES
+    session_start();
+
+
+?>
