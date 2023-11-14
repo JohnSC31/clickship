a5690b757a8f26cfded088ea2fdb3aadf592dded
Esteven <ehsteven@gmail.com>
2023-11-13 22:13:00 -0600

    modificacion de diseño

--- dbs/inventario/inventario_islas.sql
+++ dbs/inventario/inventario_islas.sql
@@ -1,35 +1,70 @@
--- Base de datos para las Islas del Caribe
+-- la base de datos para Islas
+
+-- Tabla de monedas
+CREATE TABLE monedas (
+  monedaID smallint PRIMARY KEY IDENTITY(1,1),
+  nombre varchar(30),
+  acronimo varchar(5),
+  MonedaBase bit,
+  Simbolo nchar(3)
+);
+
+-- Tabla de tipo de cambio
+CREATE TABLE tipoDeCambio (
+  tipoDeCambioID INT PRIMARY KEY IDENTITY(1,1),
+  monedaID smallint REFERENCES monedas(monedaID),
+  inicioVigencia datetime2(7),
+  finVigencia datetime2(7),
+  enabled bit,
+  precioCambio DECIMAL(10, 2)
+);
 
 -- Tabla de productos
 CREATE TABLE tipoProducto (
-	idTipoProducto INT PRIMARY KEY,
-	descripcion varchar(255)
+  idTipoProducto INT PRIMARY KEY IDENTITY(1,1),
+  descripcion varchar(255)
 );
 
--- tabla de productos
-CREATE TABLE productos_islas (
-  idProducto INT PRIMARY KEY,
+
+-- Tabla de afinidad
+CREATE TABLE afinidad (
+  idAfinidad INT PRIMARY KEY IDENTITY(1,1),
+  descripcion varchar(255) NOT NULL
+);
+
+-- Tabla de productos
+CREATE TABLE productos (
+  idProducto INT PRIMARY KEY IDENTITY(1,1),
   nombreProducto VARCHAR(255) UNIQUE NOT NULL,
-  tipoProductoID int references tipoProducto(idTipoProducto),
-  afinidad VARCHAR(255) NOT NULL
+  tipoProductoID int REFERENCES tipoProducto(idTipoProducto),
+  afinidad int REFERENCES afinidad(idAfinidad),
+  monedaID smallint REFERENCES monedas(monedaID)
 );
 
--- tabla de bodegas
-CREATE TABLE bodegas_islas (
-  idBodagaIslas INT PRIMARY KEY,
-  nombre VARCHAR(255) UNIQUE NOT NULL,
-  direccion TEXT
-);
-
--- tabla de inventario
+-- Tabla de inventario
 CREATE TABLE inventario_islas (
-  idInventarioNor INT PRIMARY KEY,
-  bodegaID INT REFERENCES bodegas_islas(idBodagaIslas),
-  productoID INT REFERENCES productos_islas(idProducto),
+  idInventarioIsla INT PRIMARY KEY IDENTITY(1,1),
+  productoID INT REFERENCES productos(idProducto),
   cantidadProducto INT
 );
 
--- Tabla de empleados en Norteamérica sin referencia
-CREATE TABLE empleados_sur(
-  idEmpleado INT PRIMARY KEY
+-- Tabla detalles de productos
+CREATE TABLE detallesProducto (
+  idDetalleProducto INT PRIMARY KEY IDENTITY(1,1),
+  productoID INT REFERENCES productos(idProducto),
+  peso DECIMAL(10, 2),
+  dimensiones VARCHAR(255),
+  descripcion TEXT,
+  monedaID smallint REFERENCES monedas(monedaID),
+  tipoDeCambioID INT REFERENCES tipoDeCambio(tipoDeCambioID)
+);
+
+-- Tabla precios históricos
+CREATE TABLE preciosHistoricos (
+  idPrecioHistorico INT PRIMARY KEY IDENTITY(1,1),
+  detalleProductoID INT REFERENCES detallesProducto(idDetalleProducto),
+  fechaInicio DATETIME NOT NULL,
+  fechaFin DATETIME,
+  precio DECIMAL(10, 2) NOT NULL,
+  tipoDeCambioID INT REFERENCES tipoDeCambio(tipoDeCambioID)
 );
--- dbs/inventario/inventario_sur.sql
+++ dbs/inventario/inventario_sur.sql
@@ -1,35 +1,69 @@
--- Base de datos para Suramérica
+-- la base de datos para Suramerica
+
+-- Tabla de monedas
+CREATE TABLE monedas (
+  monedaID smallint PRIMARY KEY IDENTITY(1,1),
+  nombre varchar(30),
+  acronimo varchar(5),
+  MonedaBase bit,
+  Simbolo nchar(3)
+);
+
+-- Tabla de tipo de cambio
+CREATE TABLE tipoDeCambio (
+  tipoDeCambioID INT PRIMARY KEY IDENTITY(1,1),
+  monedaID smallint REFERENCES monedas(monedaID),
+  inicioVigencia datetime2(7),
+  finVigencia datetime2(7),
+  enabled bit,
+  precioCambio DECIMAL(10, 2)
+);
+
+-- Tabla de afinidad
+CREATE TABLE afinidad (
+  idAfinidad INT PRIMARY KEY IDENTITY(1,1),
+  descripcion varchar(255) NOT NULL
+);
 
 -- Tabla de productos
 CREATE TABLE tipoProducto (
-	idTipoProducto INT PRIMARY KEY,
-	descripcion varchar(255)
+  idTipoProducto INT PRIMARY KEY IDENTITY(1,1),
+  descripcion varchar(255)
 );
 
--- tabla de productos
-CREATE TABLE productos_sur (
-  idProducto INT PRIMARY KEY,
+-- Tabla de productos
+CREATE TABLE productos (
+  idProducto INT PRIMARY KEY IDENTITY(1,1),
   nombreProducto VARCHAR(255) UNIQUE NOT NULL,
-  tipoProductoID int references tipoProducto(idTipoProducto),
-  afinidad VARCHAR(255) NOT NULL
+  tipoProductoID int REFERENCES tipoProducto(idTipoProducto),
+  afinidadID int REFERENCES afinidad(idAfinidad),
+  monedaID smallint REFERENCES monedas(monedaID)
 );
 
--- tabla de bodegas
-CREATE TABLE bodegas_sur (
-  idBodagaNor INT PRIMARY KEY,
-  nombre VARCHAR(255) UNIQUE NOT NULL,
-  direccion TEXT
-);
-
--- ttabla de inventario
+-- Tabla de inventario
 CREATE TABLE inventario_sur (
-  idInventarioNor INT PRIMARY KEY,
-  bodegaID INT REFERENCES bodegas_sur(idBodagaNor),
-  productoID INT REFERENCES productos_sur(idProducto),
+  idInventarioSur INT PRIMARY KEY IDENTITY(1,1),
+  productoID INT REFERENCES productos(idProducto),
   cantidadProducto INT
 );
 
--- Tabla de empleados en Norteamérica sin referencia
-CREATE TABLE empleados_sur(
-  idEmpleado INT PRIMARY KEY
+-- Tabla detalles de productos
+CREATE TABLE detallesProducto (
+  idDetalleProducto INT PRIMARY KEY IDENTITY(1,1),
+  productoID INT REFERENCES productos(idProducto),
+  peso DECIMAL(10, 2),
+  dimensiones VARCHAR(255),
+  descripcion TEXT,
+  monedaID smallint REFERENCES monedas(monedaID),
+  tipoDeCambioID INT REFERENCES tipoDeCambio(tipoDeCambioID)
+);
+
+-- Tabla precios históricos
+CREATE TABLE preciosHistoricos (
+  idPrecioHistorico INT PRIMARY KEY IDENTITY(1,1),
+  detalleProductoID INT REFERENCES detallesProducto(idDetalleProducto),
+  fechaInicio DATETIME NOT NULL,
+  fechaFin DATETIME,
+  precio DECIMAL(10, 2) NOT NULL,
+  tipoDeCambioID INT REFERENCES tipoDeCambio(tipoDeCambioID)
 );
