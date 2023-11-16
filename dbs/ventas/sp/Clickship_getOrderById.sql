a713eba3f84c1cf8842747450b5ead7a59943b09
Esteven <ehsteven@gmail.com>
2023-11-16 14:12:45 -0600

    update sp

--- dbs/ventas/sp/Clickship_getOrderById.sql
+++ dbs/ventas/sp/Clickship_getOrderById.sql
@@ -3,10 +3,15 @@ CREATE OR ALTER PROCEDURE Clickship_getOrderById
   @ordenID INT
 AS
 BEGIN
-  SELECT *
-  FROM Ordenes o
-  INNER JOIN Estados e ON o.EstadoActualID = e.EstadoID
-  INNER JOIN Moneda m ON o.MonedaID = m.MonedaID
-  INNER JOIN Paises p ON o.PaisID = p.PaisID
-  WHERE o.OrdenID = @ordenID;
+	BEGIN TRY
+	  SELECT *
+	  FROM Ordenes o
+	  INNER JOIN Estados e ON o.EstadoActualID = e.EstadoID
+	  INNER JOIN Moneda m ON o.MonedaID = m.MonedaID
+	  INNER JOIN Paises p ON o.PaisID = p.PaisID
+	  WHERE o.OrdenID = @ordenID;
+  END TRY
+	BEGIN CATCH 
+		SELECT 'Ocurrió un error al seleccionar la orden' 'Error'
+	END CATCH
 END;
--- dbs/ventas/sp/Clickship_getOrdersByClient.sql
+++ dbs/ventas/sp/Clickship_getOrdersByClient.sql
@@ -3,12 +3,17 @@ CREATE OR ALTER PROCEDURE Clickship_getOrdersByClient
   @clienteID INT
 AS
 BEGIN
-  SELECT *
-  FROM Ordenes o
-  INNER JOIN Estados e ON o.EstadoActualID = e.EstadoID
-  INNER JOIN Moneda m ON o.MonedaID = m.MonedaID
-  INNER JOIN Paises p ON o.PaisID = p.PaisID
-  WHERE o.ClienteID = @clienteID;
+	BEGIN TRY
+	  SELECT *
+	  FROM Ordenes o
+	  INNER JOIN Estados e ON o.EstadoActualID = e.EstadoID
+	  INNER JOIN Moneda m ON o.MonedaID = m.MonedaID
+	  INNER JOIN Paises p ON o.PaisID = p.PaisID
+	  WHERE o.ClienteID = @clienteID;
+	END TRY
+	BEGIN CATCH 
+		SELECT 'Ocurrió un error al seleccionar las ordenes del cliente' 'Error'
+	END CATCH
 END;
 
 exec Clickship_getOrdersByClient 1
